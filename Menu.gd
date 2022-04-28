45ea556f1284ab0eddb1f4d52978726abe947e5a
kuif321 <janbogers@leerling.wico.be>
2022-04-28 15:26:02 +0200

    coole game

---
+++ Menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+func _ready():
+	pass 
+
+
+func _on_Start_pressed():
+	pass 
+
+# get_tree().change_scene()
+func _on_Options_pressed():
+	pass # Replace with function body.
+
+
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
